8fe03338e4586bdc90386ad269ce13fc0031c3eb
r3ddy97 <venkyreddy097@gmail.com>
2018-05-16 02:23:40 +0530

    display results

--- bmtc/__init__.py
+++ bmtc/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import os
 from flask import (Flask, render_template, request)
 from webbrowser import open_new_tab
 from .web_form import SearchForm
+from .bmtc_utils import TripPlanner
 
 app = Flask(__name__)
 
@@ -17,23 +17,46 @@ def index():
 def search():
     form = SearchForm()
     if request.method == 'POST' and form.validate():
-        origin, destination = [request.form[i]
+        origin, destination = [request.form[i].strip()
                                for i in ["origin", "destination"]]
-        print(origin, destination)
+        # print(origin, destination)
+        # if not origin or not destination:
+        #     error = "Need to enter both bustops to search"
+        #     return render_template("search.html", error=error)
+
+        trip = TripPlanner(origin, destination)
+        trip_details = trip.trip_details()
+
+        if None in trip_details:
+            error = "ERR0R Couldnt find the bus stop {}".format(
+                trip_details[1])
+            return render_template("search.html", error=error)
+
+        direct_header = ["Bus", "stops", "Bus stops in between"]
+        indirect__header = ["Bus No", "Get Down Here", "Second Bus"]
+
+        if "direct" in trip_details:
+            trip_details = trip_details["direct"]
+            return render_template("search.html",
+                                   origin=origin, destination=destination,
+                                   items=trip_details, header=direct_header)
+        return render_template("indirect_search.html",
+                               origin=origin, destination=destination,
+                               items=trip_details, header=indirect__header)
     return render_template("search.html", form=form)
 
 
 @app.route("/bmtc_map")
 def bmtc_map():
-    open_new_tab("./templates/bstops_map/index.html")
+    open_new_tab("./templates/qgis2web/index.html")
     return render_template("index.html")
 
 
 @app.route("/network")
 def network():
-    return render_template("bmtc_map.html")
+    return render_template("index.html")
 
 
 @app.route("/stats")
 def stats():
-    return render_template("stats.html")
+    return render_template("index.html")
